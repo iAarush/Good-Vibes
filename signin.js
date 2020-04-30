2fdda7e22283220b73e6923d02311684b97a6003
izzyASC5 <52748903+izzyASC5@users.noreply.github.com>
2020-04-30 13:08:17 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -43,16 +43,21 @@ if (user){
   const a = document.createElement("a");
   const b = document.createElement("a")
   const c = document.createElement("a")
+  const d = document.createElement("a")
+   c.setAttribute("href", "leaning.html")
+    c. innerText = "learn something new" 
   c.setAttribute("href", "games.html")
     c. innerText = "Play Games" 
   b.setAttribute("href", "social.html")
   b.innerText = "Posts"
   a.setAttribute("href", "chat.html")
   a.innerText = "Chatroom";
+    
    
   select.append(a)
   select.append(b)
   select.append(c)
+  select.append(d)
    
 }
 })
